ed4cbd4c2ddb840f5014634dc30f909261334735
siddu1998 <samaram@ucsc.edu>
2023-10-28 13:00:01 -0700

    gpt4 upgrade

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def get_chat_completion():
     try:
         # Use OpenAI's chat model, adjust the engine as needed
         response = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
+          model="gpt-4",
           messages=chat_history
         )
         ai_response = response.choices[0].message['content'].strip()
